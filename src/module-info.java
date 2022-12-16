3bddf2d7f1224934a0aa716db2a1b34e8d05c1af
Serg <project4444@inbox.lv>
2022-12-16 11:59:17 +0200

    fixed javaFX lib issue

---
+++ src/module-info.java
@@ -0,0 +1,17 @@
+module udemyJavaProgrMasterclass {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.media;
+    requires javafx.base;
+    requires javafx.web;
+    requires javafx.swing;
+    requires javafx.fxml;
+
+
+    opens pract_lesson_.JavaFX;
+    //required to add if different applications need to run in different package, adding each package for javaFX
+    opens pract_lesson_.JavaFXToDoList;
+    opens pract_lesson_.JavaFXCSS;
+    opens pract_lesson_.JavaFXChallenge;
+    opens pract_lesson_.JavaFXChallenge.dataModel;
+}
